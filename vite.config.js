d7dad277082023646c9a401fbe005b4090b5d2c6
rafsan1 <rafsan@dcastalia.com>
2024-03-16 00:29:37 +0600

    add external files

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import react from '@vitejs/plugin-react'
-import ssr from 'vite-plugin-ssr/plugin'
+import vue from '@vitejs/plugin-vue'
+import ssr from 'vike/plugin'
 
 export default {
-  plugins: [react(), ssr()]
+  plugins: [vue(), ssr()],
 }
